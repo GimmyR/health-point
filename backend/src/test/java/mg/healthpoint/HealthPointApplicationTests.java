2e6266d2c14d1cfe5471468e3e630afe64ceddeb
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-29 16:35:51 +0300

    Add test for context loading (useful in the beginning of a project)

---
+++ backend/src/test/java/mg/healthpoint/HealthPointApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.healthpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HealthPointApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
