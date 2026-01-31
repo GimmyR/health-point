670e2b6befa72555a380485f461c3384f703501b
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-31 19:24:27 +0300

    Remove App component and use router in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './components/Root.tsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        errorElement: <div>Error page</div>,
+        children: [
+            {
+                path: "sign-in",
+                element: <div>Sign In page</div>
+            }
+        ]
+    }
+]);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router}/>
     </StrictMode>,
 )
