d9a332c6cf40f1ce7c1c5b50546c551636a4f4fc
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-02-03 16:22:47 +0300

    Edit background color of Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -26,10 +26,10 @@ export default function Header() {
     }, []);
 
     return (
-        <header className="container-fluid bg-light py-2">
+        <header className="container-fluid bg-primary py-2">
             <div className="d-flex flex-row justify-content-between align-items-center fs-3">
                 <div className="d-flex flex-row">
-                    <a href="#" className="text-dark text-decoration-none fw-bold">HealthPoint</a>
+                    <a href="#" className="text-light text-decoration-none fw-bold">HealthPoint</a>
                 </div>
                 <button type="button" className="btn btn-dark">Sign out</button>
             </div>
