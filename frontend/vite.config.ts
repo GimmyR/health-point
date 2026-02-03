741bd82167b260181130373f51e9ccf39f1662a4
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-02-03 14:13:54 +0300

    Load .env from parent directory

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, path.resolve(__dirname, '..'), 'VITE_');
-
+export default defineConfig(() => {
     return {
         plugins: [react()],
-        define: {
-            'import.meta.env': {
-                ...env
-            }
-        }
+        envDir: '../'
     };
 })
