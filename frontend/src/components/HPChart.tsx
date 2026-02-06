8a6865a8bf10cb4e74a7b82833bce5360fb85a95
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-02-06 15:51:16 +0300

    Create a component with a select to choose the type of chart to display and the chart component

---
+++ frontend/src/components/HPChart.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import LineChart from "./LineChart";
+import BarChart from "./BarChart";
+import type Parameter from "../interfaces/Parameter";
+import ChartTypeSelect from "./ChartTypeSelect";
+
+export default function HPChart({ parameter } : { parameter: Parameter }) {
+    const [chartType, setChartType] = useState("line");
+
+    return <div className="d-flex flex-column">
+        <div className="d-flex flex-row justify-content-end mb-4">
+            <ChartTypeSelect defaultValue={chartType} setChartType={setChartType}/>
+        </div>
+        {chartType == "line" ?
+            <LineChart parameter={parameter}/>
+        : chartType == "bar" ?
+            <BarChart parameter={parameter}/>
+        : null}
+    </div>
+}
