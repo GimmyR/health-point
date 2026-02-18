0faec4da089f0234c7863d30d70a064f0d806806
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-02-18 09:08:03 +0300

    Customize JWT authentication converter

--- backend/src/main/java/mg/healthpoint/configuration/SecurityConfig.java
+++ backend/src/main/java/mg/healthpoint/configuration/SecurityConfig.java
@@ -21,6 +21,8 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -53,10 +55,10 @@ public class SecurityConfig {
     			.csrf(csrf -> csrf.disable())
     			.cors(Customizer.withDefaults())
     			.authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/sign-in").permitAll()
-    														.requestMatchers("/api/patient").authenticated()
+    														.requestMatchers("/api/patient").hasRole("Patient")
     														.anyRequest().authenticated()
     			).userDetailsService(userService)
-				.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+				.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
 
         return httpSecurity.build();
 		
@@ -74,6 +76,18 @@ public class SecurityConfig {
 		
 		return httpSecurity.build();
 		
+	}
+	
+	@Bean
+	public JwtAuthenticationConverter jwtAuthenticationConverter() {
+		
+	    JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+	    grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+	    grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+	    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+	    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+	    return jwtAuthenticationConverter;
+	    
 	}
 	
 	
