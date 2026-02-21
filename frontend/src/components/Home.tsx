4086d9e17a8e4c8b6b1497291f23476e36d6c598
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-02-21 08:49:21 +0300

    Display Patient component if authenticated account is Patient and Staff component if Staff

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
 import Patient from "./Patient";
+import Staff from "./Staff";
+import { BACKEND } from "../lib/url";
 
 export default function Home() {
-    return <Patient/>
+    const [isPatient, setIsPatient] = useState(false);
+    const [isStaff, setIsStaff] = useState(false);
+
+    const verifyRole = async () => {
+        await fetch(`${BACKEND}/api/is-patient`, { headers: { "Authorization": `Bearer ${localStorage.getItem("jwtoken")}` } })
+            .then(response => response.json())
+            .then(data => setIsPatient(data));
+        
+        await fetch(`${BACKEND}/api/is-staff`, { headers: { "Authorization": `Bearer ${localStorage.getItem("jwtoken")}` } })
+            .then(response => response.json())
+            .then(data => setIsStaff(data));
+    };
+
+    useEffect(() => {
+        verifyRole();
+    }, []);
+
+    if(isPatient)
+        return <Patient/>
+
+    else if(isStaff)
+        return <Staff/>
+
+    else return null;
 }
