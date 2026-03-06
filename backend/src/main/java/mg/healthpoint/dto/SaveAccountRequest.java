c9ef872fe52f5d0d9c9ab061fdc5583132251c27
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-03-06 16:21:13 +0300

    Create DTO to save Patient

---
+++ backend/src/main/java/mg/healthpoint/dto/SaveAccountRequest.java
@@ -0,0 +1,23 @@
+package mg.healthpoint.dto;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record SaveAccountRequest(
+		@NotBlank(message = "Firstname is missing")
+		String firstname,
+		
+		@NotBlank(message = "Lastname is missing")
+		String lastname,
+		
+		@NotBlank(message = "Gender is missing")
+		String gender,
+		
+		@NotNull(message = "Date of birth is missing")
+		LocalDate dateOfBirth,
+		
+		@NotBlank(message = "Address is missing")
+		String address
+) {}
