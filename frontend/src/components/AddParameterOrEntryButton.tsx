d7d7845202a47f621994564722983096d85a57be
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-03-17 15:44:13 +0300

    Replace a tag with NavLink component and type route to add parameter

--- frontend/src/components/AddParameterOrEntryButton.tsx
+++ frontend/src/components/AddParameterOrEntryButton.tsx
@@ -1,10 +1,12 @@
+import { NavLink } from "react-router-dom";
+
 export default function AddParameterOrEntry() {
     return <div className="dropdown dropstart">
         <button type="button" className="btn btn-dark pe-3" data-bs-toggle="dropdown" aria-expanded="false">
             <i className="bi bi-plus-lg me-2"></i>Add
         </button>
         <ul className="dropdown-menu dropdown-menu-dark">
-            <li><a className="dropdown-item" href="#"><i className="bi bi-sliders2-vertical me-2"></i>Parameter</a></li>
+            <li><NavLink to="/add-parameter" className="dropdown-item"><i className="bi bi-sliders2-vertical me-2"></i>Parameter</NavLink></li>
             <li><a className="dropdown-item" href="#"><i className="bi bi-graph-up-arrow me-2"></i>Entry</a></li>
         </ul>
     </div>
