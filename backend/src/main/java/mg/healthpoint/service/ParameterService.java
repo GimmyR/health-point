39c0bc19b7792e4a1097a65d43aece90c1df71bb
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-03-21 08:43:23 +0300

    Edit method to save parameter

--- backend/src/main/java/mg/healthpoint/service/ParameterService.java
+++ backend/src/main/java/mg/healthpoint/service/ParameterService.java
@@ -2,13 +2,18 @@ package mg.healthpoint.service;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.AllArgsConstructor;
 
 import mg.healthpoint.exception.NotFoundException;
+import mg.healthpoint.dto.SaveParameterRequest;
 import mg.healthpoint.entity.Parameter;
+import mg.healthpoint.entity.Patient;
 import mg.healthpoint.repository.ParameterRepository;
+import mg.healthpoint.repository.PatientRepository;
 
 
 @Service
@@ -17,6 +22,7 @@ import mg.healthpoint.repository.ParameterRepository;
 public class ParameterService {
 	
 	private ParameterRepository parameterRepository;
+	private PatientRepository patientRepository;
 	
 	public List<Parameter> findAll() {
 		
@@ -35,7 +41,28 @@ public class ParameterService {
 		
 	}
 	
-	public Parameter save(Parameter parameter) {
+	public Parameter save(SaveParameterRequest form) throws NotFoundException, BadRequestException {
+		
+		Optional<Patient> patient = patientRepository.findById(form.patientId());
+		
+		if(form.min() != null && form.max() != null && form.min() > form.max())
+			throw new BadRequestException("Min value should be lower than max value");
+		
+		if(patient.isEmpty())
+			throw new NotFoundException("Patient not found");
+		
+		Parameter parameter = null;
+		
+		if(form.id() != null)
+			parameter = this.findUniqueById(form.id());
+		
+		else parameter = new Parameter();
+			
+		parameter.editName(form.name());
+		parameter.editUnit(form.unit());
+		parameter.editMin(form.min());
+		parameter.editMax(form.max());
+		parameter.editPatient(patient.get());
 		
 		return parameterRepository.save(parameter);
 		
