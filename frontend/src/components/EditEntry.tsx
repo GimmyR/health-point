c6af3ed77c1a5a8fca02ec65982d8bcb81e5356a
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-03-29 08:13:07 +0300

    Edit component to display page to edit entry

--- frontend/src/components/EditEntry.tsx
+++ frontend/src/components/EditEntry.tsx
@@ -1,20 +1,51 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom"
 import { BACKEND } from "../lib/url";
+import AddEntryForm from "./AddEntryForm";
+import type ParameterEntry from "../interfaces/ParameterEntry";
 
 export default function EditEntry() {
     const { id } = useParams();
-    const [ searchParams ] = useSearchParams();
+    const [searchParams] = useSearchParams();
     const navigate = useNavigate();
+    const [entry, setEntry] = useState<ParameterEntry | undefined>();
 
-    useEffect(() => {
-        fetch(`${BACKEND}/api/is-staff`, { headers: { "Authorization": `Bearer ${localStorage.getItem("jwtoken")}` } })
+    const isStaff = async () => {
+        await fetch(`${BACKEND}/api/is-staff`, { headers: { "Authorization": `Bearer ${localStorage.getItem("jwtoken")}` } })
             .then(response => response.json())
             .then(data => {
                 if(!data)
                     navigate("/");
             });
+    };
+
+    const fetchEntry = async () => {
+        await fetch(`${BACKEND}/api/entries/${id}`, {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("jwtoken")}`
+            }
+        }).then(response => response.json())
+            .then(data => setEntry(data))
+            .catch(error => console.log(error));
+    };
+
+    useEffect(() => {
+        isStaff();
+        fetchEntry();
     }, []);
 
-    return <div className="text-danger mt-5 pt-5">{ `${id} => ${searchParams.get("dt")}` }</div>
+    return (
+        <>
+            {entry && <main className="container-fluid container-lg min-vh-100 px-4 pt-5 bg-light">
+                <div className="pt-5 pb-3">
+                    <div className="d-flex flex-column align-items-center">
+                        <h1 className="fs-3 text-decoration-underline">Edit entry</h1>
+                        <div className="col-12 col-lg-4 mt-3 mt-lg-4">
+                            <AddEntryForm patientId={parseInt(searchParams.get("patient") as string)} entry={entry}/>
+                        </div>
+                    </div>
+                </div>
+            </main>}
+        </>
+    );
 }
