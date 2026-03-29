1ace73229bfc858d33ea08d01288097acb0c8eaa
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-03-29 06:34:42 +0300

    Import Bootstrap JS for dropdown button

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './components/Root.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './main.css';
 import SignIn from './components/SignIn.tsx';
 import PatientForStaff from './components/PatientForStaff.tsx';
