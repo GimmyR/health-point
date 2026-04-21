0e1a91e4253e405cc5fbc1f00aeb6c7144dfe22b
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-04-21 08:02:47 +0300

    Test method to check if authenticated user is staff or not

---
+++ backend/src/test/java/mg/healthpoint/controller/StaffRestControllerIntegrationTest.java
@@ -0,0 +1,39 @@
+package mg.healthpoint.controller;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class StaffRestControllerIntegrationTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Test
+	@WithMockUser(username = "johndoe", roles = {"Patient"})
+	public void test_isStaff() throws Exception {
+		
+		mockMvc.perform(get("/api/is-staff"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$", is(false)));
+		
+	}
+
+}
