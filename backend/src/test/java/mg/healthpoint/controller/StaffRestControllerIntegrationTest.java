b924123b38deb9c149f48d77f6940c54f0f85801
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-04-22 16:45:48 +0300

    test method to save staff

--- backend/src/test/java/mg/healthpoint/controller/StaffRestControllerIntegrationTest.java
+++ backend/src/test/java/mg/healthpoint/controller/StaffRestControllerIntegrationTest.java
@@ -2,18 +2,21 @@ package mg.healthpoint.controller;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
+import mg.healthpoint.dto.SaveAccountRequest;
+import mg.healthpoint.dto.SaveStaffRequest;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -35,5 +38,32 @@ public class StaffRestControllerIntegrationTest {
 			.andExpect(jsonPath("$", is(false)));
 		
 	}
+	
+	@Test
+	@WithMockUser(username = "admin", roles = {"Staff"})
+	public void test_saveStaff() throws Exception {
+		
+		mockMvc.perform(post("/api/staff/save")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(new SaveStaffRequest(
+					null,
+					new SaveAccountRequest(
+							"marysue", "pwdMary","Mary", "Sue", "Female", 
+							LocalDate.of(1996, 11, 11), 
+							"Itaosy Cité Akany Sambatra Lot B29, Antananarivo, Analamanga, Madagascar",
+							"033 62 667 74 / marysue@example.com"),
+					null
+				)))).andExpect(status().isCreated())
+					.andExpect(jsonPath("$", is(3)));
+		
+	}
+	
+	// tester findAllStaffs
+	
+	// tester findUniqueStaff
+	
+	// tester isAdmin
+	
+	// tester removeStaff
 
 }
