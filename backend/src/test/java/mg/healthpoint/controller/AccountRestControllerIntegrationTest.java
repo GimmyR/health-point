1f846e2443ae5b7e0eb77e180d34730269b99af8
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-05-31 13:28:53 +0300

    Make integration test on PATCH endpoint /api/accounts/edit-password/{accountId}

--- backend/src/test/java/mg/healthpoint/controller/AccountRestControllerIntegrationTest.java
+++ backend/src/test/java/mg/healthpoint/controller/AccountRestControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package mg.healthpoint.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -8,12 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import mg.healthpoint.dto.EditPasswordRequest;
 import mg.healthpoint.dto.SignInRequest;
 
 @SpringBootTest
@@ -35,6 +39,17 @@ public class AccountRestControllerIntegrationTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsBytes(new SignInRequest("johndoe", "pwdJohn"))))
 				.andExpect(status().isOk());
+		
+	}
+	
+	@Test
+	@WithMockUser(username = "admin", roles = {"Staff"})
+	public void test_editPassword() throws JsonProcessingException, Exception {
+		
+		mockMvc.perform(patch("/api/accounts/edit-password/3")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(new EditPasswordRequest("pwd123", "pwdStaff"))))
+			.andExpect(status().isOk());
 		
 	}
 
